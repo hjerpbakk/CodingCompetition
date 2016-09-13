faf193e8d61b04d9bb9f5e67f58590b35af7a74c
Runar Ovesen Hjerpbakk <hjerpbakk@gmail.com>
2016-09-13 08:31:38 +0200

    Coding Competition

---
+++ NDC2015-Competition/ItDagene2016.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NDC2015.Competition
+{
+    public class ItDagene2016
+    {
+        /// <summary>
+        /// Create a method that returns the n-th prime
+        /// given an n.
+        /// 
+        /// Make the tests pass without changing the tests
+        /// or just returning the correct result.
+        /// 
+        /// We draw two winners from the list of entries.
+        /// 
+        /// GL;HF
+        /// </summary>
+        /// <param name="n">The nth-prime</param>
+        /// <returns>Prefferably the correct result</returns>
+        public int NthPrime(int n)
+        {
+            return 0;
+        }
+    }
+}
