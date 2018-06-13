1f98df32a0ba03661c20c16044867b7fe53aa14c
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2018-06-13 09:00:48 +0200

    More desc

--- Python/Solution.py
+++ Python/Solution.py
@@ -1,11 +1,20 @@
 import re
 
-# This method takes a multiline string containing duplicates
+# This method takes a multiline string containing duplicated lines
 # and should remove the duplicated lines and concatinate
 # the string.
-# 
-# Example:
-# Given: "a\nb\na\nc"
-# Return: abc
+#
+# Example, given:
+# DIPS
+# is a great
+# place
+# DIPS
+# to work
+#
+# return:
+# DIPS is a great place to work
+#
+# @param dipsFact A multiline string with duplicates.
+# @return The distinct contatinated string.
 def distinctConcat(dips_fact):
     return ""
