25bab53d35ab94fb138738533720a353ede8551d
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2018-06-15 09:09:20 +0200

    New assignment

--- Competition/TheProblems/Problems.cs
+++ Competition/TheProblems/Problems.cs
@@ -24,45 +24,45 @@ namespace TheProblems
         [TestMethod]
         public void FirstTest()
         {
-            var median = yourSolution.Median("1, 2, 3, 4, 5");
+            var median = yourSolution.ClosestToZero("0, 1, -1, 3");
 
-            Assert.AreEqual(3, median);
+            Assert.AreEqual(0, median);
             Succeeded(1);
         }
 
         [TestMethod]
         public void SecondTest()
         {
-            var median = yourSolution.Median("45, 90, 1, 1235, 17, 3, 9");
+            var median = yourSolution.ClosestToZero("45, 90, 1, 1235, 17, 3, 9");
 
-            Assert.AreEqual(17, median);
+            Assert.AreEqual(1, median);
             Succeeded(2);
         }
 
         [TestMethod]
         public void ThirdTest()
         {
-            var median = yourSolution.Median("32, 54, 12, 64, 42");
+            var median = yourSolution.ClosestToZero("32, 54, 12, 2, -2, 64, 42");
 
-            Assert.AreEqual(42, median);
+            Assert.AreEqual(-2, median);
             Succeeded(3);
         }
 
         [TestMethod]
         public void FourthTest()
         {
-            var median = yourSolution.Median("5, 4, 3, 2, 1");
+            var median = yourSolution.ClosestToZero("5, 4, 3, 2, 1");
 
-            Assert.AreEqual(3, median);
+            Assert.AreEqual(1, median);
             Succeeded(4);
         }
 
         [TestMethod]
         public void FifthTest()
         {
-            var median = yourSolution.Median("90, 72, 17, 15, 19");
+            var median = yourSolution.ClosestToZero("90, 72, 17, 15, 19");
 
-            Assert.AreEqual(19, median);
+            Assert.AreEqual(15, median);
             Succeeded(5);
         }
 
