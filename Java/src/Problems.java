8d5d9f39a28a013ab6c4d02d68c32ebf46c21c7d
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2018-09-12 08:11:55 +0200

    Added Java assignment

--- Java/src/Problems.java
+++ Java/src/Problems.java
@@ -6,51 +6,41 @@ import java.nio.file.Paths;
 
 public class Problems {
     //region
-    private Solution solution;
+    private Solution yourSolution;
 
     @Before
     public void setUp() throws Exception {
-        solution = new Solution();
+        yourSolution = new Solution();
     }
     //endregion
 
     @Test
     public void DIPS_Fact_1() throws Exception {
-        int median  = solution.closestToZero("0,1,-1,3");
-
-        Assert.assertEquals(0, median);
+        Assert.assertEquals(0, yourSolution.distance("GGACTGA", "GGACTGA"));
         Succeeded(1);
     }
 
     @Test
     public void DIPS_Fact_2() throws Exception {
-        int median  = solution.closestToZero("45,90,1,1235,17,3,9");
-
-        Assert.assertEquals(1, median);
+        Assert.assertEquals(1, yourSolution.distance("A", "G"));
         Succeeded(2);
     }
 
     @Test
     public void DIPS_Fact_3() throws Exception {
-        int median  = solution.closestToZero("32,54,12,2,-2,64,42");
-
-        Assert.assertEquals(-2, median);
+        Assert.assertEquals(2, yourSolution.distance("AG", "CT"));
         Succeeded(3);
     }
 
     @Test
     public void DIPS_Fact_4() throws Exception {
-        int median  = solution.closestToZero("-5,-4,-3,-2,-1");
-
-        Assert.assertEquals(-1, median);
+        Assert.assertEquals(2, yourSolution.distance("ACCAGGG", "ACTATGG"));
         Succeeded(4);
     }
 
     @Test
     public void DIPS_Fact_5() throws Exception {
-        int median  = solution.closestToZero("90,72,17,15,19");
-
-        Assert.assertEquals(15, median);
+        Assert.assertEquals(9, yourSolution.distance("GGACGGATTCTG", "AGGACGGATTCT"));
         Succeeded(5);
     }
 
