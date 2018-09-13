80e262e1c2fb5a8c2c74e19bc9ca0743cc69085d
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2018-09-13 09:15:20 +0200

    Added Python

--- Python/Problems.py
+++ Python/Problems.py
@@ -1,5 +1,5 @@
 from TestBase import TestBase
-from Solution import distance
+from Solution import isLeapYear
 import unittest
 
 # These are the tests
@@ -10,23 +10,23 @@ class Problems(unittest.TestCase):
 		cls.testBase = TestBase()
 
 	def test_dips_fact_1(self):
-		self.assertEqual(0, distance("GGACTGA", "GGACTGA"))
+		self.assertEqual(True, isLeapYear(2000))
 		Problems.testBase.Succeeded(1)
 
 	def test_dips_fact_2(self):
-		self.assertEqual(1, distance("A", "G"))
+		self.assertEqual(False, isLeapYear(2015))
 		Problems.testBase.Succeeded(2)
 
 	def test_dips_fact_3(self):
-		self.assertEqual(2, distance("AG", "CT"))
+		self.assertEqual(True, isLeapYear(1996))
 		Problems.testBase.Succeeded(3)
 
 	def test_dips_fact_4(self):
-		self.assertEqual(2, distance("ACCAGGG", "ACTATGG"))
+		self.assertEqual(False, isLeapYear(2100))
 		Problems.testBase.Succeeded(4)
 
 	def test_dips_fact_5(self):
-		self.assertEqual(9, distance("GGACGGATTCTG", "AGGACGGATTCT"))
+		self.assertEqual(False, isLeapYear(1997))
 		Problems.testBase.Succeeded(5)
 
 	@classmethod
